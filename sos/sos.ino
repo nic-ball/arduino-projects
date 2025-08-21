4caa0dc15705b61836b1c6dd36dc15da5e362289
nic-ball <nicholasball147@gmail.com>
2025-08-21 22:23:46 +0100

    Added sos

---
+++ sos/sos.ino
@@ -0,0 +1,47 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(1500);
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(1500);
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(1500);
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(300);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(3000);
+}
